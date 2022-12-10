72718bd72902ec417545d905042ab51e4c830286
Ozan Atici <ozanact@gmail.com>
2022-12-09 23:40:30 -0500

    Project has been completed

--- src/Circle.java
+++ src/Circle.java
@@ -6,7 +6,10 @@
  */
 
 //implement Shape interface and provide abstract method implementation
-public class Circle{
+public class Circle implements Shape{
+
+
+
 
     /*
     Define an instance variable called as pi and give initial value as 3.14
@@ -14,8 +17,7 @@ public class Circle{
     double pi
      */
 
-
-
+    private static final double pi = 3.14;
 
     /*
     Define an instance variable for Circle called as radius
@@ -23,7 +25,15 @@ public class Circle{
     double radius
      */
 
+    private double radius;
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 
     /*
     Override area and perimeter methods here
@@ -35,8 +45,22 @@ public class Circle{
     Basically it is => 2 * pi * radius
     */
 
+    @Override
+    public double area() {
+        return pi * radius * radius;
+    }
 
+    @Override
+    public double perimeter() {
+        return 2 * (pi * radius);
+    }
 
-    //Override toString() method here
+//Override toString() method here
 
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
 }
